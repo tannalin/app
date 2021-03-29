80a5208748a8903d879b7e64caf5ffdf1e4654e2
zhilinIPD19 <lin.zhilin0@gmail.com>
2021-03-29 02:00:17 -0400

    hello

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Person zHilin = new Person("ZHilin");
+        System.out.println(zHilin.getName());
+    }
+}
